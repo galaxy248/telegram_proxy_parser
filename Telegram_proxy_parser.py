9dde0b1fc7b72d2c503320df0d06b6f9f8b0435b
deep_dreamer <mra541728@gmail.com>
2023-08-09 09:36:05 +0330

    updating the engine from "tkinter" to "pyqt5".

--- Telegram_proxy_parser.py
+++ Telegram_proxy_parser.py
@@ -1,189 +1,157 @@
-import re, os
-import tkinter as tk
-import pyglet
+import re, os, sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit,\
+                            QLabel, QPushButton, QMessageBox
+from PyQt5.QtGui import QIcon, QClipboard, QFont, QFontDatabase
+from PyQt5 import uic
+import webbrowser
 
 
-def on_button_click(button_id):
-    global server_text
-    global server_button
-    global port_text
-    global port_button
-    global secret_text
-    global secret_button
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        # set default values
+        self.__path = os.path.dirname(os.path.abspath(__file__))
+        self.__proxy = {
+            "server": None,
+            "port": None,
+            "secret": None
+        }
+        # regex pattern for validate proxy link
+        self.__pattern = r"https:\/\/t\.me\/proxy\?server=((2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)\.(2[01234]\d|25[0-5]|1\d{2}|\d{1,2})\.(2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)\.(2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)|((\w|\.|\-|\_)+))&port=(\d{1,4}|[1-5]\d{4}|6[0-5][0-5][0-3][0-5])&secret=(.+)"
 
-    if button_id == 1: # confrim
+        uic.loadUi(self.__path + "\\app.ui", self)
+        self.setMinimumSize(600, 500)
+        self.setWindowIcon(QIcon(self.__path + "\\image\\Telegram_logo.png"))
+
+        self.__find_all_components()
+        self.__load_styles()
+        self.__set_functions()
+
+        # self.show()
+
+    def __find_all_components(self):
+        self.header: QLabel = self.findChild(QLabel, "header")
+
+        self.text_0: QLabel = self.findChild(QLabel, "text")
+
+        self.text_sr: QLabel = self.findChild(QLabel, "text_sr")
+        self.text_sc: QLabel = self.findChild(QLabel, "text_sc")
+        self.text_p: QLabel = self.findChild(QLabel, "text_p")
+
+        self.link_input: QLineEdit = self.findChild(QLineEdit, "link_input")
+
+        self.confrim_btn: QPushButton = self.findChild(QPushButton, "confrim_btn")
+        self.clear_btn: QPushButton = self.findChild(QPushButton, "clear_btn")
+        
+        self.port_btn: QPushButton = self.findChild(QPushButton, "port_btn")
+        self.server_btn: QPushButton = self.findChild(QPushButton, "server_btn")
+        self.secret_btn: QPushButton = self.findChild(QPushButton, "secret_btn")
+
+
+    def __get_link_and_verify(self):
         # get link and validate it
-        link = link_input.get()
-        if re.match(pattern, link) is None: # doesn't match
-            # create popup window and show error
-            top = tk.Toplevel(window)
-            top.geometry("650x250")
-            top.title("Error")
-            error_icon = tk.PhotoImage(file=dir_path + "\\image\\error.png")
-            top.iconphoto(False, error_icon)
-
-            tk.Label(top, text="Your link is not valied !", fg="red", font=("VazirMatn", 20)).pack()
-            tk.Label(top, text="Please review the link.\nIf you sure that link is coorect,\nplease creating an issue in Gitea or Github to fix the bug.", font=("VazirMatn, 18")).pack()
-            tk.Label(top, text="Gitea ID: deep_dreamer\nGithub ID: galaxy248", fg="#32458F", font=("VazirMatn, 18")).pack()
-            tk.Button(
-                top,
-                text="OK",
-                width=8,
-                height=1,
-                bg="blue",
-                fg="yellow",
-                font=("Roboto", 10),
-                command=lambda: top.destroy()
-            ).pack()
-        
+        link = self.link_input.text().strip()
+        if re.match(self.__pattern, link) is None: # doesn't match
+            # create a message box and show the error
+            msg_box = QMessageBox()
+            msg_box.setIcon(QMessageBox.Critical)
+            msg_box.setWindowIcon(QIcon(self.__path + "\\image\\error.png"))
+            msg_box.setWindowTitle("Invalied Link")
+            msg_box.setText("Your link is not valied !\nPlease review the link.\nIf you sure that link is coorect,\
+                            \nplease click on \"open\" button to creating an issue in Github to fix the bug.\
+                            \nGithub ID: galaxy248")
+            msg_box.setStyleSheet("QMessageBox { font: 14pt \"Open Sans Medium\" rgb(0, 0, 127);\
+                                   background-color: rgb(221, 239, 255) }")
+            msg_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Open | QMessageBox.Help)
+            btn = msg_box.exec_()
+            if btn == QMessageBox.Ok:
+                pass
+            elif btn == QMessageBox.Open:
+                webbrowser.open_new_tab("https://github.com/galaxy248/telegram_proxy_parser/issues")
+            elif btn == QMessageBox.Help:
+                # create a message box and show the help page
+                help_msg_box = QMessageBox()
+                help_msg_box.setIcon(QMessageBox.Information)
+                help_msg_box.setWindowIcon(QIcon(self.__path + "\\image\\info.png"))
+                help_msg_box.setWindowTitle("Valied Format Link")
+                help_msg_box.setText("Valied Format Link:\n(https|http)://(telegram|t).me/proxy?server=(SERVER IP|LINK)&port=(NUMBER LESS THAN 65,535)&secret=(KEY EXPRESSION)")
+                help_msg_box.setStyleSheet("QMessageBox { font: 14pt \"Open Sans Medium\" rgb(0, 0, 127); background-color: rgb(221, 239, 255)}")
+                help_msg_box.setStandardButtons(QMessageBox.Ok)
+                help_msg_box.exec_()
+            else:
+                pass
         else:
-            sections = re.findall(pattern, link)
-            proxy["server"] = sections[0][0]
-            proxy["port"] = sections[0][7]
-            proxy["secret"] = sections[0][8]
+            sections = re.findall(self.__pattern, link)
+            self.__proxy["server"] = sections[0][0]
+            self.__proxy["port"] = sections[0][7]
+            self.__proxy["secret"] = sections[0][8]
 
-            server_text = tk.Label(window, text="Server", bg="#BDBDBD", font=("VazirMatn", "18"))
-            server_text.place(x=20, y=195)
-            server_button = tk.Button(
-                window,
-                text="Copy",
-                width=8,
-                height=1,
-                bg="blue",
-                fg="yellow",
-                font=("Roboto", 12),
-                command=lambda: copy_button_click(1)
-            )
-            server_button.place(x=120, y=200)
+    def __clear_link_input(self):
+        self.link_input.clear()
+        self.__proxy["server"] = None
+        self.__proxy["secret"] = None
+        self.__proxy["port"] = None
 
-            port_text = tk.Label(window, text="Port", bg="#BDBDBD", font=("VazirMatn", "18"))
-            port_text.place(x=20, y=250)
-            port_button = tk.Button(
-                window,
-                text="Copy",
-                width=8,
-                height=1,
-                bg="blue",
-                fg="yellow",
-                font=("Roboto", 12),
-                command=lambda: copy_button_click(2)
-            )
-            port_button.place(x=120, y=250)
+    def __server_copy_button_clicked(self):
+        clipboard: QClipboard = QApplication.clipboard()
+        clipboard.clear()
+        clipboard.setText(self.__proxy["server"])
 
-            secret_text = tk.Label(window, text="Secret", bg="#BDBDBD", font=("VazirMatn", "18"))
-            secret_text.place(x=20, y=300)
-            secret_button = tk.Button(
-                window,
-                text="Copy",
-                width=8,
-                height=1,
-                bg="blue",
-                fg="yellow",
-                font=("Roboto", 12),
-                command=lambda: copy_button_click(3)
-            )
-            secret_button.place(x=120, y=300)
+    def __secret_copy_button_clicked(self):
+        clipboard: QClipboard = QApplication.clipboard()
+        clipboard.clear()
+        clipboard.setText(self.__proxy["secret"])
+
+    def __port_copy_button_clicked(self):
+        clipboard: QClipboard = QApplication.clipboard()
+        clipboard.clear()
+        clipboard.setText(self.__proxy["port"])
+
+    def __set_functions(self):
+        self.confrim_btn.clicked.connect(self.__get_link_and_verify)
+        self.clear_btn.clicked.connect(self.__clear_link_input)
         
-    elif button_id == 2: # clear
-        link_input.delete(0, tk.END)
-        try:
-            server_text.destroy()
-            server_button.destroy()
+        self.server_btn.clicked.connect(self.__server_copy_button_clicked)
+        self.secret_btn.clicked.connect(self.__secret_copy_button_clicked)
+        self.port_btn.clicked.connect(self.__port_copy_button_clicked)
 
-            port_text.destroy()
-            port_button.destroy()
+    def __load_styles(self):
+        # load fonts
+        QFontDatabase.addApplicationFont(self.__path + "\\font\\Colakind.ttf")
+        QFontDatabase.addApplicationFont(self.__path + "\\font\\PartyConfetti.ttf")
+        QFontDatabase.addApplicationFont(self.__path + "\\font\\Roboto.ttf")
+        QFontDatabase.addApplicationFont(self.__path + "\\font\\OpenSans-Medium.ttf")
+        QFontDatabase.addApplicationFont(self.__path + "\\font\\Vazirmatn.ttf")
+        # set styles
+        header_font: QFont = QFont("ColaKind", 30)
+        self.header.setFont(header_font)
 
-            secret_text.destroy()
-            secret_button.destroy()
+        style = "QLabel { font: 18pt Roboto; color: rgb(0, 0, 127); }"
+        self.text_0.setStyleSheet(style)
+        
+        style = "QLabel { font: 28pt \"Party Confetti\"; color: rgb(0, 0, 127); }"
+        self.text_p.setStyleSheet(style)
+        self.text_sc.setStyleSheet(style)
+        self.text_sr.setStyleSheet(style)
 
-        except Exception as e:
-            pass
-
-
-def copy_button_click(button_id):
-    global proxy
-    # paste arguments to clipboard
-    if button_id == 1:
-        window.clipboard_clear()
-        window.clipboard_append(proxy["server"])
-        window.update()
-    elif button_id == 2:
-        window.clipboard_clear()
-        window.clipboard_append(proxy["port"])
-        window.update()
-    elif button_id == 3:
-        window.clipboard_clear()
-        window.clipboard_append(proxy["secret"])
-        window.update()
-    else:
-        pass
+        style = "QPushButton { border-radius: 15px; background-color: rgb(0, 68, 255); \
+                 color: rgb(255, 255, 127); font: 22pt \"Party Confetti\" } \
+                 QPushButton:pressed { background-color: rgb(0, 132, 255); border: 3px solid #55aaff }"
+        self.confrim_btn.setStyleSheet(style)
+        self.clear_btn.setStyleSheet(style)
+        
+        style = "QPushButton { border-radius: 15px; background-color: rgb(0, 68, 255); \
+                color: rgb(255, 255, 127); font: 22pt \"Party Confetti\"; \
+                width: 208px; height: 56px; } \
+                QPushButton:pressed { background-color: rgb(0, 132, 255); border: 3px solid #55aaff }"
+        self.port_btn.setStyleSheet(style)
+        self.secret_btn.setStyleSheet(style)
+        self.server_btn.setStyleSheet(style)
 
 
-# regex pattern for validate proxy link
-pattern = r"https:\/\/t\.me\/proxy\?server=((2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)\.(2[01234]\d|25[0-5]|1\d{2}|\d{1,2})\.(2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)\.(2[01234]\d|25[0-5]|1\d{2}|\d{2}|\d)|((\w|\.|\-|\_)+))&port=(\d{1,4}|[1-5]\d{4}|6[0-5][0-5][0-3][0-5])&secret=(.+)"
-# add fonts to app
-dir_path = os.path.dirname(os.path.abspath(__file__))
-pyglet.options['win32_gdi_font'] = True
-pyglet.font.add_file(dir_path + "\\font\\Vazirmatn.ttf")
-pyglet.font.add_file(dir_path + "\\font\\Colakind.ttf")
-pyglet.font.add_file(dir_path + "\\font\\Roboto.ttf")
+app = QApplication(sys.argv)
 
-# set default values
-proxy = {
-    "server": None,
-    "port": None,
-    "secret": None
-}
-server_text = None
-server_button = None
-port_text = None
-port_button = None
-secret_text = None
-secret_button = None
+win = MainWindow()
+win.show()
 
-# create window and set configs
-window = tk.Tk()
-window.geometry("512x400")
-window.configure(background="#BDBDBD")
-window.title("Telegram Proxy")
-app_icon = tk.PhotoImage(file=dir_path + "\\image\\Telegram_logo.png")
-window.iconphoto(False, app_icon)
-
-# set header
-header = tk.Label(window, text="Telegram Proxy", fg="#00A2E8", bg="#EFE4B0", width="512", font=("ColaKind", 40))
-header.pack()
-
-# get proxy link
-text0 = tk.Label(window, text="Please Type Your Telegram Proxy Link", bg="#BDBDBD", font=("VazirMatn", "14"))
-text0.place(x=10, y=80)
-
-link_input = tk.Entry(window, fg="yellow", bg="#7F7F7F", width=80)
-link_input.config(highlightthickness=1, highlightbackground="#8F7500")
-link_input.place(x=10, y=110)
-link_input.get()
-
-confrim_button = tk.Button(
-    window,
-    text="Confrim",
-    width=10,
-    height=0,
-    bg="blue",
-    fg="yellow",
-    font=("Roboto", 10),
-    command=lambda: on_button_click(1)
-)
-confrim_button.place(x=10, y=135)
-
-clear_button = tk.Button(
-    window,
-    text="Clear",
-    width=10,
-    height=0,
-    bg="blue",
-    fg="yellow",
-    font=("Roboto", 10),
-    command=lambda: on_button_click(2)
-)
-clear_button.place(x=110, y=135)
-
-window.mainloop()
+sys.exit(app.exec_())
